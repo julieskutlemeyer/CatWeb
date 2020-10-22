83715d9aebc0359f04a4aa038341765a8ba03b77
Jonathan Johannessen <jonathsj@stud.ntnu.no>
2020-10-22 13:42:50 +0200

    Auto stash before merge of "setup-mongo" and "origin/setup-mongo"

--- adoptcat_backend/routes/api.js
+++ adoptcat_backend/routes/api.js
@@ -4,27 +4,27 @@ const Cat = require('../models/cat');
 
 router.get('/cat', (req, res, next) => {
 
-  Cat.find({})
-    .then(data => res.json(data))
-    .catch(next)
+    Cat.find({})
+        .then(data => res.json(data))
+        .catch(next)
 });
 
 router.post('/cat', (req, res, next) => {
-  if(req.body.name){
-    Cat.create(req.body)
-      .then(data => res.json(data))
-      .catch(next)
-  }else {
-    res.json({
-      error: "The input field is empty"
-    })
-  }
+    if (req.body.name) {
+        Cat.create(req.body)
+            .then(data => res.json(data))
+            .catch(next)
+    } else {
+        res.json({
+            error: "The input field is empty"
+        })
+    }
 });
 
 router.delete('/cat/:id', (req, res, next) => {
-  Cat.findOneAndDelete({"_id": req.params.id})
-    .then(data => res.json(data))
-    .catch(next)
+    Cat.findOneAndDelete({ "_id": req.params.id })
+        .then(data => res.json(data))
+        .catch(next)
 });
 
 module.exports = router;
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios').default;
 
 function App() {
+  const [data, setData] = useState("")
+  const [dat, setDat] = useState(0)
+
+  axios.get('http://it2810-10.idi.ntnu.no:5000/api/cat').then((res: any) => {
+    console.log(res);
+    setData(res.data[dat].name)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +19,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {data}
         <a
           className="App-link"
           href="https://reactjs.org"
