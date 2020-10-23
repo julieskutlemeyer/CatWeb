5dfd7b68632c00780a66d493e9aaf50f130d4da3
Aleksander Karlsson <aleksander.mats.karlsson@gmail.com>
2020-10-23 11:55:57 +0200

    #9 :lipstick: added react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-function App() {
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Button from 'react-bootstrap/Button';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="top-level-container">
+      <Jumbotron>
+        <h1 className="header">
+          Welcom to Adopt-a-Cat :3
+        </h1>
+      </Jumbotron>
+      <Row>
+        <Col> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum a dolorum ratione dicta veritatis molestiae. Suscipit incidunt exercitationem nemo iure fugiat, error tenetur quod, maxime nulla excepturi, reiciendis consectetur. Quia.</Col>
+        <Col> Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt autem velit recusandae consequatur voluptatibus et deleniti provident, ullam quod facere esse, consequuntur neque eum dolor eos commodi, tenetur aspernatur dolorem reprehenderit! Dignissimos a accusantium iste facilis exercitationem, quidem nam praesentium fuga nesciunt culpa laborum et nostrum id! Perspiciatis illo at aperiam placeat possimus rerum aspernatur dolore laborum? Ab itaque optio facilis harum ad quas dicta autem voluptas illum sit nobis necessitatibus debitis quidem rem reiciendis, blanditiis, dolore beatae! Perferendis magnam, voluptatum porro nulla similique, debitis hic consequatur libero quis rem possimus! Ab dolor rem facere non beatae ratione ex ut.</Col>
+      </Row>
+
+
+      <h2>Buttons</h2>
+      <Button/>
+      <h2>Toasts</h2>
+      <Button/>
+    </Container>
   );
-}
+};
+
+
 
 export default App;
