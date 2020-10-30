2ea54ccc6a98f5911dc7c922e73385ce1e47955c
Aleksander Karlsson <aleksander.mats.karlsson@gmail.com>
2020-10-30 21:21:26 +0100

    #11 :white_check_mark: Added tests

--- src/Cats/CatsList.tsx
+++ src/Cats/CatsList.tsx
@@ -100,10 +100,9 @@ export default function CatsList() {
                         <svg id="favoriteStarIcon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={() => { dispatch(putLike({ id: post._id, likes: post.post.likes + 1 })) }}>
                             <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
                         </svg>
-                        <p>Likes: {post.post.likes}</p>
+                        <div id="likes"><pre>Likes: </pre> <p id="likenr">{post.post.likes}</p></div>
                     </div>
                 </div>
-
             </div>
         </Col>
 
